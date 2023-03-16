d7f3047c9c72f5af3498f5f17eadf9ca34eef686
Svitlana Synytsia <svetlankogr@gmail.com>
2023-03-16 23:50:50 +0100

    services

---
+++ src/services/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const rickAndMortyApi = axios.create({
+  baseURL: 'https://rickandmortyapi.com/api',
+});
+
+export const getCharacters = async page => {
+  const { data } = await rickAndMortyApi.get('/character', {
+    params: {
+      page,
+    },
+  });
+  return data;
+};
+export const getCharacterByQuery = async (query, page) => {
+  const { data } = await rickAndMortyApi.get('/character', {
+    params: {
+      name: query,
+      page,
+    },
+  });
+  return data;
+};
+
+export const getCharacterById = async id => {
+  const { data } = await rickAndMortyApi.get(`/character/${id}`);
+  return data;
+};
