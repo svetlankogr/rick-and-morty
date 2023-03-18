51bd3c7b44ad06f6d761aaaa545c5d94dc99be91
Svitlana Synytsia <svetlankogr@gmail.com>
2023-03-18 18:24:07 +0100

    add loading and error

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -5,59 +5,66 @@ import { CharacterList } from 'components/CharacterList/CharacterList';
 import { Logo } from 'components/Logo/Logo';
 import { SearchForm } from 'components/SearchForm/SearchForm';
 import { PaginationBlock } from 'components/PaginationBlock/PaginationBlock';
+import { Loader } from 'components/Loader/Loader';
 
 import css from './Characters.module.css';
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
-  const [filteredCharacters, setFilteredCharacters] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
   const characterName = searchParams.get('query') ?? '';
   const [page, setPage] = useState(1);
   const [pageCount, setPageCount] = useState(0);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     if (!characterName) {
-      getCharacters(page).then(({ results, info }) => {
-        setCharacters(results);
-        setPageCount(info.pages);
-      });
+      getCharacters(page)
+        .then(({ results, info }) => {
+          setCharacters(results);
+          setPageCount(info.pages);
+        })
+        .catch(error => setError(error.message))
+        .finally(() => setLoading(false), setError(null));
       return;
     }
-    getCharacterByQuery(characterName, page).then(({ results, info }) => {
-      setFilteredCharacters(results);
-      setPageCount(info.pages);
-    });
+    getCharacterByQuery(characterName, page)
+      .then(({ results, info }) => {
+        setCharacters(results);
+        setPageCount(info.pages);
+      })
+      .catch(error => setError(error.message))
+      .finally(() => setLoading(false), setError(null));
   }, [characterName, page]);
 
   const onFormSubmit = e => {
     e.preventDefault();
     setSearchParams({ query: searchQuery });
-    setFilteredCharacters('');
     setPage(1);
   };
 
   const onChange = e => setSearchQuery(e.target.value);
   const onChangePage = (_, num) => setPage(num);
   return (
-    <div className={css.charactersPage}>
-      <Logo />
-      <SearchForm
-        searchQuery={searchQuery}
-        onFormSubmit={onFormSubmit}
-        onChange={onChange}
-      />
-      {filteredCharacters.length > 0 ? (
-        <CharacterList characters={filteredCharacters} />
-      ) : (
-        <CharacterList characters={characters} />
-      )}
-      <PaginationBlock
-        pageCount={pageCount}
-        page={page}
-        onChange={onChangePage}
-      />
-    </div>
+    <>
+      {loading && <Loader />}
+      <div className={css.charactersPage}>
+        <Logo />
+        <SearchForm
+          searchQuery={searchQuery}
+          onFormSubmit={onFormSubmit}
+          onChange={onChange}
+        />
+        {error ? <p>{error}</p> : <CharacterList characters={characters} />}
+        <PaginationBlock
+          pageCount={pageCount}
+          page={page}
+          onChange={onChangePage}
+        />
+      </div>
+    </>
   );
 };
 export default Characters;
